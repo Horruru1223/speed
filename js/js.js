067fdc7085befb38e1337839460441a140f1dce4
horruru1223 <aeleschenko@edu.hse.ru>
2022-03-18 16:52:46 +0300

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,199 @@
+
+$(document).ready(function() {
+  $('.rectangle1').click(function(){
+      $('.ladybug').css("display", "block");
+      $('.tights').css("display", "block");
+      $('.kitty').css("display", "block");
+      // $('.sketch').css("display", "none"");
+      // $('.pancakes').css("display", "none"");
+      // $('.watermelon').css("display", "none");
+      // $('.two').css("display", "none");
+      // $('.sausage').css("display", "none");
+      // $('.ticket').css("display", "none");
+      // $('.umbrella').css("display", "none");
+      // $('.pomegrante').css("display", "none");
+  });
+});
+
+$(document).ready(function() {
+  $('.rectangle2').click(function(){
+      $('.sketch').css("display", "block");
+      $('.pancakes').css("display", "block");
+      $('.watermelon').css("display", "block");
+      // $('.two').css("display", "none");
+      // $('.sausage').css("display", "none");
+      // $('.ticket').css("display", "none");
+      // $('.umbrella').css("display", "none");
+      // $('.pomegrante').css("display", "none");
+      // $('.ladybug').css("display", "none");
+      // $('.tights').css("display", "none");
+      // $('.kitty').css("display", "none");
+  });
+});
+
+$(document).ready(function() {
+  $('.rectangle3').click(function(){
+      $('.two').css("display", "block");
+      $('.sausage').css("display", "block");
+      // $('.sketch').css("display", "none"");
+      // $('.pancakes').css("display", "none"");
+      // $('.watermelon').css("display", "none");
+      // $('.ticket').css("display", "none");
+      // $('.umbrella').css("display", "none");
+      // $('.pomegrante').css("display", "none");
+      // $('.ladybug').css("display", "none");
+      // $('.tights').css("display", "none");
+      // $('.kitty').css("display", "none");
+  });
+});
+
+$(document).ready(function() {
+  $('.rectangle4').click(function(){
+      $('.ticket').css("display", "block");
+      $('.umbrella').css("display", "block");
+      $('.pomegrante').css("display", "block");
+      // $('.section5').css("display", "none");
+      // // $('.sketch').css("display", "none"");
+      // $('.pancakes').css("display", "none"");
+      // $('.watermelon').css("display", "none");
+      // $('.ladybug').css("display", "none");
+      // $('.tights').css("display", "none");
+      // $('.kitty').css("display", "none");
+      // $('.two').css("display", "none");
+      // $('.sausage').css("display", "none");
+  });
+});
+
+$(document).ready(function() {
+  $('.umbrella').click(function(){
+    $('.video-bg').css("display", "block");
+    $('.umbrella').css("filter", "grayscale(0%)");
+    $('.sea').css("display", "none");
+    $(".ticket").css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.ticket').click(function(){
+    $('.sea').css("display", "block");
+    $(".ticket").css("filter", "grayscale(0%)");
+    $('.video-bg').css("display", "none");
+    $('.umbrella').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.sausage').click(function(){
+    $('.school').css("display", "block");
+    $(".sausage").css("filter", "grayscale(0%)");
+    // $('.two').css("display", "none");
+    $('.two').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.watermelon').click(function(){
+    $('.video-lake').css("display", "block");
+    $(".watermelon").css("filter", "grayscale(0%)");
+    $('.dacha').css("display", "none");
+    $('.pancakes').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.pancakes').click(function(){
+    $('.dacha').css("display", "block");
+    $('.pancakes').css("filter", "grayscale(0%)");
+    $('.video-lake').css("display", "none");
+    $('.watermelon').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.ladybug').click(function(){
+    $('.flower1').css("display", "block");
+    $('.flower2').css("display", "block");
+    $('.flower3').css("display", "block");
+    $('.ladybug').css("filter", "grayscale(0%)");
+    $('.window').css("display", "none");
+    $('.tights').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.tights').click(function(){
+    $('.window').css("display", "block");
+    $('.tights').css("filter", "grayscale(0%)");
+    $('.flower1').css("display", "none");
+    $('.flower2').css("display", "none");
+    $('.flower3').css("display", "none");
+    $('.ladybug').css("filter", "grayscale(100%)");
+  });
+});
+
+$(document).ready(function() {
+  $('.kitty').click(function(){
+    $('.texnumber1').css("display", "block");
+    $('.textmonth1').css("display", "block");
+    $('.kitty').css("filter", "grayscale(0%)");
+    $('.flower1').css("display", "none");
+    $('.flower2').css("display", "none");
+    $('.flower3').css("display", "none");
+    $('.ladybug').css("filter", "grayscale(100%)");
+    $('.window').css("display", "none");
+    $('.tights').css("filter", "grayscale(100%)");
+    $('.text2').css("display", "none");
+  });
+});
+
+$(document).ready(function() {
+  $('.sketch').click(function(){
+    $('.fire1').css("display", "block");
+    $('.fire2').css("display", "block");
+    $('.fire3').css("display", "block");
+    $('.fire4').css("display", "block");
+    $('.fire5').css("display", "block");
+    $('.candle1').css("display", "block");
+    $('.candle2').css("display", "block");
+    $('.candle3').css("display", "block");
+    $('.candle4').css("display", "block");
+    $('.candle5').css("display", "block");
+    $(".sketch").css("filter", "grayscale(0%)");
+    $('.dacha').css("display", "none");
+    $('.pancakes').css("filter", "grayscale(100%)");
+  });
+});
+
+// var video = document.getElementById("myVideo");
+
+// $(document).ready(function() {
+//   $('.ticket').click(function(){
+//     $('.sea').css("display", "block");
+//     $(".ticket").css("filter", "grayscale(0%)");
+//   });
+// });
+//
+// $(document).ready(function() {
+//   $('.ticket').click(function(){
+//     $('.sea').css("display", "block");
+//     $(".ticket").css("filter", "grayscale(0%)");
+//   });
+// });
+
+
+
+
+// $(document).ready(function(){
+//   console.log('I hate tomatoes.');
+// });
+
+// $(document).ready(function(){
+//   $(".pomegrante").click(function(){
+//     console.log("pribet")
+//   });
+// });
+// $(document).ready(function(){
+//  $("img").click(function(){
+//   alert("it works!");
+//  });
+// });
